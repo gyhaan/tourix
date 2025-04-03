7ae9fee5a35910a0222ac05e162518963e0b72f0
Christianib003 <christianib003@gmail.com>
2025-04-03 19:28:45 +0200

    implement setup and teardown methods for widget testing

---
+++ test/widget/login_screen_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tourix_app/screens/login.dart';
+import '../helpers/test_helpers.dart';
+
+void main() {
+  group('Login Screen Widget Tests', () {
+    setUpAll(() {
+      TestHelpers.setupAll();
+    });
+
+    tearDownAll(() {
+      TestHelpers.tearDownAll();
+    });
+
+    
+  });
+}
